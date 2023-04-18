c99fd636cd93371b9968ce98e0d960fc08131a44
AJOYSR <jucse28.384@gmail.com>
2023-04-18 10:52:56 +0600

    adding final files

---
+++ src/Components/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Todos from "./Todos";
+import style from "./home.module.css";
+import NewTodo from "./NewTodo";
+import { v4 as uuidv4 } from "uuid";
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (todo) => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), todo }];
+    });
+  };
+
+  const handleRemoveTodo = (id) => {
+    setTodos((prevTodos) => {
+      const fileteredTodos = todos.filter((todo) => todo.id !== id);
+      return fileteredTodos;
+    });
+  };
+  
+  return (
+    <div className={style.container}>
+      <h1 style={{ color: "white" }}>Todo App</h1>
+      <NewTodo onAddTodo={handleAddTodo} />
+      <Todos todos={todos} onRemoveTodo={handleRemoveTodo} />
+    </div>
+  );
+}
